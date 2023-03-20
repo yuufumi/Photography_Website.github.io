5831cb14d53d5f45f5313555b6643c6f96e00e34
Youcef BENALI <105224275+yuufumi@users.noreply.github.com>
2023-03-20 15:23:08 +0100

    responsive navbar added

--- photography_website/components/Navbar.js
+++ photography_website/components/Navbar.js
@@ -1,10 +1,37 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Link from 'next/link'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 const Navbar = () => {
+  const [nav, setNav]= useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
   return (
-    <nav className='bg-white'>
-      
-    </nav>
+    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300 '>
+        <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>            
+            <Link href="/"><h1 className='font-bold text-4xl '>Captur</h1></Link>
+            <ul className='hidden sm:flex'>
+                <Link href="/" className='p-4'><li>Home</li></Link>
+                <Link href="/gallery"  className='p-4'><li>Gallery</li></Link>
+                <Link href="/portfolio" className='p-4'><li>Work</li></Link>
+                <Link href="/contact" className='p-4'><li>Contact</li></Link>
+            </ul>
+            {/* Mobile button */}
+            <button className='block sm:hidden z-10' onClick={handleNav}>
+                {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/> }
+            </button>
+            {/* Mobile menu */}
+            <div className={nav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300' : 'sm:hidden absolute top-[-100%] left-0 right-0 bottom-0 flex justify-center items-center h-full  v-screen bg-black text-center ease-in duration-300'}>
+                <ul>
+                    <Link href="/"><li className='p-4 text-4xl hover:text-gray-500'>Home</li></Link>
+                    <Link href="/gallery"><li className='p-4 text-4xl hover:text-gray-500'>Gallery</li></Link>
+                    <Link href="/portfolio"><li className='p-4 text-4xl hover:text-gray-500'>Work</li></Link>
+                    <Link href="/contact"><li className='p-4 text-4xl hover:text-gray-500'>Contact</li></Link>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- photography_website/pages/index.js
+++ photography_website/pages/index.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Hero from '../components/Hero'
-import Navbar from '../components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <Navbar />
     <Hero heading='Captur Photography' message='I capture moments in nature and keep them alive.'  />
     </>
   )
