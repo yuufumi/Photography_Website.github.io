676993820a3cdd50e306b310428066c70dfb689e
Youcef BENALI <105224275+yuufumi@users.noreply.github.com>
2023-03-20 07:36:35 +0100

    Hero section added and styled

---
+++ photography_website/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Hero({heading, message}) {
+  return (
+    
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img z-[2]'>
+      {/* Overlay */}
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70'/>
+      <div className='p-5 text-white z-[2] mt-[-5rem] '>
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className='py-5 text-xl font-medium'>{message}</p>
+        <button className='px-8 py-2 border-2 '>Book</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero;
---
+++ photography_website/pages/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime velit vitae consequuntur molestiae, iusto eos expedita, ad voluptas similique consequatur quidem. Possimus eveniet nostrum quia saepe cum iusto vitae dolor.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatum exercitationem culpa assumenda laboriosam, illo, non, possimus id architecto placeat illum nemo nulla. Voluptatum eos in unde necessitatibus est explicabo.</p>
+        </>
+      );
+}
+ 
+export default About;
